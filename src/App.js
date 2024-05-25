1dd9f047f1c787822c3dd10adc51b3acc3688249
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-05-25 10:59:43 +0300

    files changed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
  import './App.css';
 import Header from './Header';
+import Content from './Content';
 
 function App() {
-  const handleName= function() {
-    const names=['Ivan','Pesho','Gosho']
-    const randonName=Math.floor(Math.random()*names.length);
-    return names[randonName];
-  }
-
+  
   return (
     <div className="App">
       <Header />
+      <Content />
     </div>
   );
 }
--- src/Content.js
+++ src/Content.js
@@ -1,6 +1,12 @@
 const Content=()=> {
+    const handleName= function() {
+        const names=['Ivan','Pesho','Gosho']
+        const randonName=Math.floor(Math.random()*names.length);
+        return names[randonName];
+      }    
+    
     return (
-       <p></p>
+       <p>Hello {handleName}!</p>
     )
 }
 
