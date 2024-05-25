e1e2d81823dae0abb70a060b6e68ab6a8e9002a9
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-05-25 10:28:30 +0300

    files changed

--- src/Content.js
+++ src/Content.js
@@ -1,7 +1,18 @@
 const Content=()=> {
+          const handleName= function() {
+          const names=['Ivan','Pesho','Gosho']
+          const randonName=Math.floor(Math.random()*names.length);
+          return names[randonName];
+        }
     return (
-       <p></p>
+    <main>
+         <p>
+             Hello {handleName()}!
+         </p>
+    </main>   
+       
     )
 }
+
 
 export default Content
